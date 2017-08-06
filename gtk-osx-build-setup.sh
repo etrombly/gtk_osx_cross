10f58017bbf5d6b5ea0e61d1257021152ddd51c0
Eric Trombly <etrombly@yahoo.com>
2017-08-06 17:13:50 +0000

    add osx-build-setup script back

---
+++ gtk-osx-build-setup.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#
+# Script that sets up jhbuild for GTK+ OS X building. Run this to
+# checkout jhbuild and the required configuration.
+#
+# Copyright 2007, 2008, 2009 Imendio AB
+#
+# Run this whenever you want to update jhbuild or the jhbuild setup;
+# it is safe to run it repeatedly. Note that it overwrites
+# ~/.jhbuildrc however. Custom settings should be kept in
+# ~/.jhbuildrc-custom.
+#
+# You need Mac OS X 10.4 or newer and Xcode 2.5 or newer. Make sure
+# you have subversion (svn) installed, 10.5 has it by default.
+#
+# Quick HOWTO:
+#
+# sh gtk-osx-build-setup.sh
+#
+# jhbuild bootstrap
+# jhbuild build meta-gtk-osx-bootstrap
+# jhbuild build
+#
+# See http://live.gnome.org/GTK%2B/OSX/Building for more information.
+#
+
+SOURCE=$HOME/Source
+BASEURL="https://git.gnome.org/browse/gtk-osx/plain/"
+
+do_exit()
+{
+    echo $1
+    exit 1
+}
+
+get_moduleset_from_git()
+{
+    curl -ks "$BASEURL/modulesets-stable/$1" -o $SOURCE/jhbuild/modulesets/$1 || \
+	do_exit "Unable to download $1"
+}
+
+mkdir -p $SOURCE 2>/dev/null || do_exit "The directory $SOURCE could not be created. Check permissions and try again."
+
+#If ~/.jhbuildrc is a link, assume that it's to a gtk-osx repository
+#and don't touch it.
+if [ ! -L $HOME/.jhbuildrc ]; then
+    echo "Installing jhbuild configuration..."
+    curl -ks $BASEURL/jhbuildrc-gtk-osx -o $HOME/.jhbuildrc || do_exit "Didn't get jhbuildrc"
+    curl -ks $BASEURL/jhbuildrc-gtk-osx-fw-10.4 -o $HOME/.jhbuildrc-fw-10.4
+    curl -ks $BASEURL/jhbuildrc-gtk-osx-cfw-10.4 -o $HOME/.jhbuildrc-cfw-10.4
+    curl -ks $BASEURL/jhbuildrc-gtk-osx-cfw-10.4u -o $HOME/.jhbuildrc-cfw-10.4u
+    curl -ks $BASEURL/jhbuildrc-gtk-osx-fw-10.4-test -o $HOME/.jhbuildrc-fw-10.4-test
+fi
+if [ ! -f $HOME/.jhbuildrc-custom ]; then
+    curl -ks $BASEURL/jhbuildrc-gtk-osx-custom-example -o $HOME/.jhbuildrc-custom || do_exit "Didn't get jhbuildrc-custom"
+fi
+
+echo "Installing gtk-osx moduleset files..."
+MODULES="bootstrap.modules gtk-osx-bootstrap.modules gtk-osx.modules gtk-osx-gstreamer.modules gtk-osx-gtkmm.modules gtk-osx-javascript.modules gtk-osx-network.modules gtk-osx-python.modules gtk-osx-random.modules gtk-osx-themes.modules gtk-osx-unsupported.modules"
+
+mkdir -p "${SOURCE}/jhbuild/modulesets"
+for m in $MODULES; do
+    get_moduleset_from_git $m
+done
+
+echo "Done."
